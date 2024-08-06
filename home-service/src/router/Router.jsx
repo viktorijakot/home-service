4229850677510c1f90a44322e38dcf8f74a495b6
Viktorija.K <viktorija.kotaite@gmail.com>
2024-08-06 19:19:14 +0300

    Header/topbar created

--- home-service/src/router/Router.jsx
+++ home-service/src/router/Router.jsx
@@ -2,7 +2,6 @@ import { createBrowserRouter } from "react-router-dom";
 import HomePage from "../pages/HomePage/HomePage";
 import ServicesPage from "../pages/ServicesPage/ServicesPage";
 import AboutUsPage from "../pages/AboutUsPage/AboutUsPage";
-
 import NotSignedLayout from "../layouts/NotSignedLayout";
 import { ROUTES } from "./Routes";
 
@@ -15,14 +14,22 @@ const router = createBrowserRouter([
       {
         path: ROUTES.BASE,
         element: <HomePage />,
+        navLinkInfo: "Home",
       },
       {
         path: ROUTES.SERVICES,
         element: <ServicesPage />,
+        navLinkInfo: "Services",
       },
       {
         path: ROUTES.ABOUT_US,
         element: <AboutUsPage />,
+        navLinkInfo: "About Us",
+      },
+      {
+        path: ROUTES.LOGIN,
+        element: <>Login</>,
+        navLinkInfo: "Login",
       },
     ],
   },
--- home-service/src/router/Routes.jsx
+++ home-service/src/router/Routes.jsx
@@ -2,4 +2,5 @@ export const ROUTES = {
   BASE: "/",
   SERVICES: "services",
   ABOUT_US: "about-us",
+  LOGIN: "login",
 };
