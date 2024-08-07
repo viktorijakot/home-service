3b0628bfb0112b9db92cedd9a54806a57a874610
Viktorija.K <viktorija.kotaite@gmail.com>
2024-08-07 11:18:31 +0300

    search input and button

--- home-service/src/pages/HomePage/HomePage.jsx
+++ home-service/src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,8 @@
+import Button from "../../components/Button/Button";
 import PageTitle from "../../components/PageTitle/PageTitle";
 import styles from "./homePage.module.css";
+import Search from "../../assets/icons/search.svg";
+import TextField from "../../components/TextField/TextField";
 
 function HomePage() {
   return (
@@ -10,6 +13,18 @@ function HomePage() {
         headlinePartThree={"Near You"}
         text="Explore Best Home Service & Repair near you"
       />
+      <div className={styles.searchContainer}>
+        <TextField
+          type="text"
+          name="search"
+          id="search"
+          placeholder="Search"
+          onChange={() => {}}
+        />
+        <Button type="submit" shape="circle" onClick={() => {}}>
+          <img src={Search} alt="search" />
+        </Button>
+      </div>
     </div>
   );
 }
