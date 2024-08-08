94aee442f2431baf343a1d0f4680c252d8563524
Viktorija.K <viktorija.kotaite@gmail.com>
2024-08-08 14:03:12 +0300

    improved categoryCard component

--- home-service/src/components/CategoryCard/CategoryCard.jsx
+++ home-service/src/components/CategoryCard/CategoryCard.jsx
@@ -1,9 +1,24 @@
 import styles from "./categoryCard.module.css";
 import PropTypes from "prop-types";
+import classNames from "classnames/bind";
+import { generatePath, useNavigate, useParams } from "react-router-dom";
+import { ROUTES } from "../../router/Routes";
 
-function CategoryCard({ imgUrl, name }) {
+const cx = classNames.bind(styles);
+
+function CategoryCard({ imgUrl, name, shape }) {
+  const { category } = useParams();
+  const navigate = useNavigate();
+
+  const categoryPath = generatePath(ROUTES.SEARCH_CATEGORY, {
+    category: name,
+  });
+
   return (
-    <div className={styles.container}>
+    <div
+      className={cx("container", shape, { active: category === name })}
+      onClick={() => navigate(categoryPath)}
+    >
       <img src={imgUrl} alt={name} />
       {name}
     </div>
@@ -13,6 +28,7 @@ function CategoryCard({ imgUrl, name }) {
 CategoryCard.propTypes = {
   imgUrl: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
+  shape: PropTypes.oneOf(["rectangle", "square"]),
 };
 
 export default CategoryCard;
