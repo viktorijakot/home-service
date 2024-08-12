8d3d9c94db8cbc4751301e547838929821dca6e2
Viktorija.K <viktorija.kotaite@gmail.com>
2024-08-12 12:57:50 +0300

    added user context to header

--- home-service/src/components/Header/Header.jsx
+++ home-service/src/components/Header/Header.jsx
@@ -5,11 +5,15 @@ import { ROUTES } from "../../router/Routes";
 import NavigationOptions from "../NavigationOptions/NavigationOptions";
 import { getNavLinks } from "../../router/PagesConfig";
 import Button from "../Button/Button";
+import { useContext } from "react";
+import { UserContext } from "../context/UserContext";
+import Avatar from "../Avatar/Avatar";
 
 function Header() {
   const navLinksToDisplay = getNavLinks().map(({ path, text }) => (
     <NavigationOptions key={path} link={path} linkTitle={text} />
   ));
+  const { user } = useContext(UserContext);
 
   const navigate = useNavigate();
 
@@ -25,9 +29,13 @@ function Header() {
         </NavLink>
         {navLinksToDisplay}
       </nav>
-      <Button type="button" onClick={handleClick}>
-        Login / Sign Up
-      </Button>
+      {user ? (
+        <Avatar>{user.email[0]}</Avatar>
+      ) : (
+        <Button type="button" onClick={handleClick}>
+          Login / Sign Up
+        </Button>
+      )}
     </header>
   );
 }
