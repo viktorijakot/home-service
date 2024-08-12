04763933d8d63a8c2fac5946a11f781997a49f3d
Viktorija.K <viktorija.kotaite@gmail.com>
2024-08-12 12:58:11 +0300

    created loginRegisterCard component

---
+++ home-service/src/components/LoginRegisterCard/LoginRegisterCard.jsx
@@ -0,0 +1,18 @@
+import { useLocation } from "react-router-dom";
+import styles from "./loginRegisterCard.module.css";
+import { ROUTES } from "../../router/Routes";
+import Login from "../Auth/Login/Login";
+import Register from "../Auth/Register/Register";
+
+function LoginRegisterCard() {
+  const location = useLocation();
+
+  return (
+    <div className={styles.container}>
+      {location.pathname === ROUTES.LOGIN && <Login />}
+      {location.pathname === ROUTES.REGISTER && <Register />}
+    </div>
+  );
+}
+
+export default LoginRegisterCard;
