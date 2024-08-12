f6260428088007169c69d9172c00c28737a42866
Viktorija.K <viktorija.kotaite@gmail.com>
2024-08-12 12:56:50 +0300

    created avatar component

---
+++ home-service/src/components/Avatar/Avatar.jsx
@@ -0,0 +1,46 @@
+import styles from "./avatar.module.css";
+import PropTypes from "prop-types";
+import { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { UserContext } from "../../components/context/UserContext";
+import { ROUTES } from "../../router/Routes";
+import Button from "../Button/Button";
+
+const Avatar = ({ children }) => {
+  const { logout } = useContext(UserContext);
+  const navigate = useNavigate();
+  const [showLogout, setShowLogout] = useState(false);
+
+  const handleLogoutClick = () => {
+    // Log out the user
+    logout();
+
+    // Redirect to the login page
+    navigate(ROUTES.LOGIN);
+  };
+
+  const toggleLogoutVisibility = () => {
+    setShowLogout(!showLogout);
+  };
+
+  return (
+    <>
+      <div className={styles.avatar} onClick={toggleLogoutVisibility}>
+        {children}
+      </div>
+      {showLogout && (
+        <div className={styles.logout}>
+          <Button type="button" onClick={handleLogoutClick}>
+            Log out
+          </Button>
+        </div>
+      )}
+    </>
+  );
+};
+
+Avatar.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default Avatar;
