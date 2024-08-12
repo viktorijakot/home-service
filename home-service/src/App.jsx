53dbafae59a0ff9bd4abcbfffbc6f8fb0b1e96d7
Viktorija.K <viktorija.kotaite@gmail.com>
2024-08-12 12:56:09 +0300

    created user context

--- home-service/src/App.jsx
+++ home-service/src/App.jsx
@@ -1,9 +1,14 @@
 import { RouterProvider } from "react-router-dom";
 import "./App.css";
 import router from "./router/Router";
+import { UserProvider } from "./components/context/UserContext";
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <UserProvider>
+      <RouterProvider router={router} />
+    </UserProvider>
+  );
 }
 
 export default App;
