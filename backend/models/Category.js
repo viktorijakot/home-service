de0920a3dadba67e153db789a1ac67dc5b385392
Viktorija.K <viktorija.kotaite@gmail.com>
2024-08-14 13:20:32 +0300

    left only name in categorySchema

--- backend/models/Category.js
+++ backend/models/Category.js
@@ -5,18 +5,6 @@ const categorySchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  bgcolor: {
-    hex: {
-      type: String,
-      default: "#FFFFFF",
-    },
-  },
-  icon: {
-    url: {
-      type: String,
-      default: "http://example.com/default-icon.png",
-    },
-  },
 });
 
 module.exports = mongoose.model("Category", categorySchema);
