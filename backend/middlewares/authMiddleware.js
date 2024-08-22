cfc0a65583ada4851b58c22390b8923984d72e4c
Viktorija.K <viktorija.kotaite@gmail.com>
2024-08-22 10:39:23 +0300

    prettier eslint extensions

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -1,20 +1,20 @@
-const jwt = require("jsonwebtoken");
+const jwt = require('jsonwebtoken');
 
 const authMiddleware = (req, res, next) => {
   const authHeader = req.headers.authorization;
 
-  if (!authHeader || !authHeader.startsWith("Bearer ")) {
-    res.status(401).send({ error: "Not authenticated" });
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    res.status(401).send({ error: 'Not authenticated' });
     return;
   }
 
   try {
-    const token = authHeader.split(" ")[1];
+    const token = authHeader.split(' ')[1];
     const payload = jwt.verify(token, process.env.JWT_SECRET);
     req.currentUser = payload;
   } catch (err) {
     console.error(err);
-    res.status(401).send({ error: "Not authenticated" });
+    res.status(401).send({ error: 'Not authenticated' });
     return;
   }
 
