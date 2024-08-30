d39d5305ae47546dfecc61506eeab19f0ed17fdc
Viktorija.K <viktorija.kotaite@gmail.com>
2024-08-30 11:17:44 +0300

    created bookings api requests

---
+++ home-service/src/api/bookingApi.ts
@@ -0,0 +1,11 @@
+import { Booking, BookingRequest } from '@/types/booking';
+import { axiosInstance } from './axiosConfig';
+
+export const postBooking = async (booking: BookingRequest) => {
+  await axiosInstance.post('/bookings', booking);
+};
+
+export const fetchBookings = async (): Promise<Booking[]> => {
+  const response = await axiosInstance.get('/bookings');
+  return await response.data;
+};
