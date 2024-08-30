8fc6246c440daae7b82d9dac8a02ffb30f8dd7d9
Viktorija.K <viktorija.kotaite@gmail.com>
2024-08-30 11:19:02 +0300

    created DatePicker component

---
+++ home-service/src/components/others/DatePicker/DatePicker.tsx
@@ -0,0 +1,22 @@
+import ReactDatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import { startOfToday } from 'date-fns';
+
+interface DatePickerProps {
+  selectedDate: Date | null;
+  onDateChange: (date: Date | null) => void;
+}
+
+const DatePicker = ({ selectedDate, onDateChange }: DatePickerProps) => {
+  return (
+    <ReactDatePicker
+      selected={selectedDate}
+      onChange={(date) => onDateChange(date)}
+      dateFormat="yyyy-MM-dd"
+      inline
+      minDate={startOfToday()}
+    />
+  );
+};
+
+export default DatePicker;
