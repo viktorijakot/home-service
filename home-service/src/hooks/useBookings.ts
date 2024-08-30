ba052b72d11b75e339853308f77e9559fa6ca61e
Viktorija.K <viktorija.kotaite@gmail.com>
2024-08-30 11:16:56 +0300

    created useBookings hook

---
+++ home-service/src/hooks/useBookings.ts
@@ -0,0 +1,11 @@
+import { useQuery } from "@tanstack/react-query";
+import { BOOKINGS_KEY } from "./useBookingRequest";
+import { fetchBookings } from "@/api/bookingApi";
+
+
+export const useBookings = () => {
+  return useQuery({
+    queryKey: [BOOKINGS_KEY],
+    queryFn: fetchBookings,
+  });
+};
