06ed1c02756236884e8c6a9a40f1297c66425108
Viktorija.K <viktorija.kotaite@gmail.com>
2024-08-30 11:17:15 +0300

    created useBookingRequest hook

---
+++ home-service/src/hooks/useBookingRequest.ts
@@ -0,0 +1,15 @@
+
+import { UseMutationResult, useMutation, useQueryClient } from '@tanstack/react-query';
+import { ErrorResponse } from 'react-router-dom';
+import { BookingRequest } from '@/types/booking';
+import { postBooking } from '@/api/bookingApi';
+
+export const BOOKINGS_KEY = 'BOOKINGS'
+export const useBookingRequest = (): UseMutationResult<void, ErrorResponse, BookingRequest, unknown> => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: postBooking,
+    onSuccess: () => queryClient.invalidateQueries({ queryKey: [BOOKINGS_KEY] }),
+  });
+};
