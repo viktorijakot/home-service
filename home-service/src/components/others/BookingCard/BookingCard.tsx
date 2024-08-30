b8024f95a5e8b235f7b96932c64bf116f684b2ae
Viktorija.K <viktorija.kotaite@gmail.com>
2024-08-30 19:11:14 +0300

    created bookingCard component

---
+++ home-service/src/components/others/BookingCard/BookingCard.tsx
@@ -0,0 +1,48 @@
+import { Booking } from '@/types/bookings';
+import { Business } from '@/types/businesses';
+import { format } from 'date-fns';
+import styles from './bookingCard.module.css';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
+
+interface BookingCardProps {
+  business: Business;
+  booking: Booking;
+}
+
+const BookingCard = ({ business, booking }: BookingCardProps) => {
+  const { images, contactPerson, address, name } = business;
+  const { date, time } = booking;
+
+  return (
+    <div className={styles.container}>
+      <img className={styles.image} src={images[0].url} alt={name} />
+      <div className={styles.infoContainer}>
+        <h4>{name}</h4>
+        <p className={cx('contactPerson', 'infoDisplayed')}>
+          <img src="../src/assets/icons/user.png" />
+          {contactPerson}
+        </p>
+        <p className={cx('infoDisplayed')}>
+          <img src="../src/assets/icons/location.png" />
+          {address}
+        </p>
+        <p className={cx('infoDisplayed')}>
+          <img src="../src/assets/icons/calendar.png" />
+          <span>
+            Service on: <b>{format(new Date(date), 'dd-MMMM-yyyy')}</b>
+          </span>
+        </p>
+        <p className={cx('infoDisplayed')}>
+          <img src="../src/assets/icons/clock.png" />
+          <span>
+            Service on: <b>{format(new Date(time), 'hh:mm a')}</b>
+          </span>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default BookingCard;
