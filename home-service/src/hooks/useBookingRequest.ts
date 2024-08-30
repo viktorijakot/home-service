ac13a8bba7f773bef173acd6b8789e917f5dd0f4
Viktorija.K <viktorija.kotaite@gmail.com>
2024-08-30 19:31:42 +0300

    added token

--- home-service/src/hooks/useBookingRequest.ts
+++ home-service/src/hooks/useBookingRequest.ts
@@ -3,13 +3,24 @@ import { UseMutationResult, useMutation, useQueryClient } from '@tanstack/react-
 import { ErrorResponse } from 'react-router-dom';
 import { BookingRequest } from '@/types/booking';
 import { postBooking } from '@/api/bookingApi';
+import useLocalStorage from './useLocalStorage';
 
 export const BOOKINGS_KEY = 'BOOKINGS'
+
 export const useBookingRequest = (): UseMutationResult<void, ErrorResponse, BookingRequest, unknown> => {
   const queryClient = useQueryClient();
+  const [token] = useLocalStorage<string | null>('token', null);
 
   return useMutation({
-    mutationFn: postBooking,
-    onSuccess: () => queryClient.invalidateQueries({ queryKey: [BOOKINGS_KEY] }),
+    mutationFn: (booking: BookingRequest) => {
+      if (token) {
+        return postBooking(booking, token);
+      } else {
+        throw new Error('No token available');
+      }
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: [BOOKINGS_KEY] });
+    },
   });
 };
--- home-service/src/hooks/useBookings.ts
+++ home-service/src/hooks/useBookings.ts
@@ -1,11 +1,19 @@
 import { useQuery } from "@tanstack/react-query";
 import { BOOKINGS_KEY } from "./useBookingRequest";
 import { fetchBookings } from "@/api/bookingApi";
+import useLocalStorage from "./useLocalStorage";
 
 
 export const useBookings = () => {
+  const [token] = useLocalStorage<string | null>('token', null);
+
   return useQuery({
     queryKey: [BOOKINGS_KEY],
-    queryFn: fetchBookings,
+    queryFn: () => {
+      if (!token) {
+        throw new Error('No authentication token found');
+      }
+      return fetchBookings(token);
+    },
   });
-};
+}
