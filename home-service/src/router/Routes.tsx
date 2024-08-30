2145c4c1946843282875ac7b9ecf792b61734b5b
Viktorija.K <viktorija.kotaite@gmail.com>
2024-08-30 19:12:15 +0300

    added userBookingPage to the router

--- home-service/src/router/Routes.tsx
+++ home-service/src/router/Routes.tsx
@@ -1,8 +1,9 @@
 export const ROUTES = {
-  BASE: "/",
-  SERVICES: "services",
-  ABOUT_US: "about-us",
-  LOGIN: "/login",
-  REGISTER: "/register",
-  SEARCH_CATEGORY: "/search/:category",
+  BASE: '/',
+  SERVICES: 'services',
+  ABOUT_US: 'about-us',
+  LOGIN: '/login',
+  REGISTER: '/register',
+  SEARCH_CATEGORY: '/search/:category',
+  MY_BOOKINGS: '/my-bookings',
 };
