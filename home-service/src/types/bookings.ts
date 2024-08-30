fe71918f087f2292e5bee8c7c24b541e4f0eacf2
Viktorija.K <viktorija.kotaite@gmail.com>
2024-08-30 19:09:20 +0300

    added booking types

---
+++ home-service/src/types/bookings.ts
@@ -0,0 +1,16 @@
+import { Business } from './businesses';
+
+export type Status = 'confirmed' | 'pending' | 'cancelled';
+
+export interface BookingRequest {
+  businessId: Business['_id'];
+  date: Date;
+  time: string;
+  userEmail: string;
+  userName: string;
+  status: Status;
+}
+
+export interface Booking extends BookingRequest {
+  _id: string;
+}
