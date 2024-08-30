54ac1a485e901bc84409be25126d675cdb4420b5
Viktorija.K <viktorija.kotaite@gmail.com>
2024-08-30 11:19:26 +0300

    created TimeSlotPicker component

---
+++ home-service/src/components/others/TimeSlotPicker/TimeSlotPicker.tsx
@@ -0,0 +1,42 @@
+import { TimeSlot } from '@/types/calendar';
+import { format, isBefore } from 'date-fns';
+import styles from './timeSlotPicker.module.css';
+import classNames from 'classnames/bind';
+import { useState } from 'react';
+
+const cx = classNames.bind(styles);
+
+interface TimeSlotPickerProps {
+  slots: TimeSlot[];
+  onSlotSelect: (slot: TimeSlot) => void;
+}
+
+const TimeSlotPicker = ({ slots, onSlotSelect }: TimeSlotPickerProps) => {
+  const [activeSlot, setActiveSlot] = useState<TimeSlot | null>(null);
+  const now = new Date();
+
+  const handleClick = (slot: TimeSlot) => {
+    onSlotSelect(slot);
+    setActiveSlot(slot);
+  };
+
+  return (
+    <div className={styles.timeSlotContainer}>
+      {slots.map((slot) => {
+        const isPast = isBefore(new Date(slot.startTime), now); // Ensure startTime is a Date object
+        return (
+          <button
+            key={new Date(slot.startTime).getTime()} // Ensure startTime is a Date object
+            className={cx('timeSlot', { booked: slot.isBooked, active: slot === activeSlot, disabled: isPast })}
+            onClick={() => handleClick(slot)}
+            disabled={slot.isBooked || isPast}
+          >
+            {format(new Date(slot.startTime), 'h:mm a')} {/* Ensure startTime is a Date object */}
+          </button>
+        );
+      })}
+    </div>
+  );
+};
+
+export default TimeSlotPicker;
