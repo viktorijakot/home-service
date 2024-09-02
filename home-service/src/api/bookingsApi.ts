1cf058db5eb2c3665a6b3b656dc7ac0cb7d1dc35
Viktorija.K <viktorija.kotaite@gmail.com>
2024-09-02 15:28:05 +0300

    removed authorization

--- home-service/src/api/bookingsApi.ts
+++ home-service/src/api/bookingsApi.ts
@@ -1,23 +1,12 @@
-import { Booking } from '@/types/bookings';
-import { axiosInstance } from './axiosConfig';
-import useLocalStorage from '@/hooks/useLocalStorage';
+import { Booking } from "@/types/bookings";
+import { axiosInstance } from "./axiosConfig";
 
-export const fetchBookings = async (token: string): Promise<Booking[]> => {
-  const response = await axiosInstance.get('/bookings', {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
-
+export const fetchBookings = async (): Promise<Booking[]> => {
+  const response = await axiosInstance.get("/bookings");
   return response.data;
 };
 
-export const fetchUserBookings = async (token: string, email: string): Promise<Booking[]> => {
-  const response = await axiosInstance.get(`/bookings/user/${email}`, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
-
+export const fetchUserBookings = async (email: string): Promise<Booking[]> => {
+  const response = await axiosInstance.get(`/bookings/user/${email}`);
   return response.data;
 };
