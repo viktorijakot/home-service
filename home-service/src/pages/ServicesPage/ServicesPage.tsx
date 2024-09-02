255d70e0ca8a41bd894361ffd7f2ee7c19379dd1
Viktorija.K <viktorija.kotaite@gmail.com>
2024-09-02 19:19:09 +0300

    removed booking from service page

--- home-service/src/pages/ServicesPage/ServicesPage.tsx
+++ home-service/src/pages/ServicesPage/ServicesPage.tsx
@@ -1,11 +1,5 @@
-import Booking from "@/components/others/Booking/Booking";
-
 const ServicesPage = () => {
-  return (
-    <div>
-      <Booking id="66bc887671dd74cd1d8ae978" />
-    </div>
-  );
+  return <div>Services</div>;
 };
 
 export default ServicesPage;
