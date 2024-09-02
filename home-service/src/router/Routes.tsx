4a4caea9d58a1cf78ef41256424a833bfaefffe7
Viktorija.K <viktorija.kotaite@gmail.com>
2024-09-02 12:53:16 +0300

    added new route

--- home-service/src/router/Routes.tsx
+++ home-service/src/router/Routes.tsx
@@ -1,8 +1,9 @@
 export const ROUTES = {
-  BASE: "/",
-  SERVICES: "services",
-  ABOUT_US: "about-us",
-  LOGIN: "/login",
-  REGISTER: "/register",
-  SEARCH_CATEGORY: "/search/:category",
+  BASE: '/',
+  SERVICES: 'services',
+  ABOUT_US: 'about-us',
+  LOGIN: '/login',
+  REGISTER: '/register',
+  SEARCH_CATEGORY: '/search/:category',
+  SEARCH_BUSINESS: '/business/:id',
 };
