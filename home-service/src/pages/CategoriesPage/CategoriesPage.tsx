65c3bb6ed19e90dd3bd1243cdb65064ccdfe6aff
Viktorija.K <viktorija.kotaite@gmail.com>
2024-09-03 19:29:13 +0300

    added functionality to the button

--- home-service/src/pages/CategoriesPage/CategoriesPage.tsx
+++ home-service/src/pages/CategoriesPage/CategoriesPage.tsx
@@ -1,18 +1,28 @@
-import { useParams } from 'react-router-dom';
-import styles from './categoriesPage.module.css';
-import CategoryCard from '@/components/others/CategoryCard/CategoryCard';
-import BusinessesCard from '@/components/others/BusinessesCard/BusinessesCard';
-import Button from '@/components/common/Button/Button';
-import { useBusinesses } from '@/hooks/useBusinesses';
-import { useCategories } from '@/hooks/useCategories';
+import { generatePath, useNavigate, useParams } from "react-router-dom";
+import styles from "./categoriesPage.module.css";
+import CategoryCard from "@/components/others/CategoryCard/CategoryCard";
+import BusinessesCard from "@/components/others/BusinessesCard/BusinessesCard";
+import Button from "@/components/common/Button/Button";
+import { useBusinesses } from "@/hooks/useBusinesses";
+import { useCategories } from "@/hooks/useCategories";
+import { Business } from "@/types/businesses";
+import { ROUTES } from "@/router/Routes";
 
 const CategoriesPage = () => {
   const { category } = useParams();
   const { data } = useBusinesses();
-  const businesses = data ?? [];
   const { data: categories } = useCategories();
+  const businesses = data ?? [];
+  const navigate = useNavigate();
 
   const filteredBusinesses = businesses.filter((business) => business.category === category);
+
+  const handleClick = (id: Business["_id"]) => {
+    const businessPath = generatePath(ROUTES.BUSINESSES_ID, {
+      id,
+    });
+    navigate(businessPath);
+  };
 
   return (
     <div className={styles.container}>
@@ -27,7 +37,7 @@ const CategoriesPage = () => {
         <div className={styles.businessesCardList}>
           {filteredBusinesses.map((business) => (
             <BusinessesCard key={business._id} business={business}>
-              <Button type="button" onClick={() => {}}>
+              <Button type="button" onClick={() => handleClick(business._id)}>
                 Book now
               </Button>
             </BusinessesCard>
