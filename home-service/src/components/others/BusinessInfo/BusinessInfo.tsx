1c0fe8ef9f8c3d580b94153dd1d6f51626092eeb
Viktorija.K <viktorija.kotaite@gmail.com>
2024-09-03 19:30:17 +0300

    finished component

--- home-service/src/components/others/BusinessInfo/BusinessInfo.tsx
+++ home-service/src/components/others/BusinessInfo/BusinessInfo.tsx
@@ -1,27 +1,47 @@
-import { Business } from '@/types/businesses';
-import styles from './businessInfo.module.css';
+import { Business } from "@/types/businesses";
+import styles from "./businessInfo.module.css";
+import { HiOutlineMail } from "react-icons/hi";
+import { CiLocationOn } from "react-icons/ci";
 
 const BusinessInfo = ({ business }: { business: Business }) => {
   if (!business) {
     return;
   }
 
-  const { about, images, _id } = business;
+  const { about, imageUrls, _id, name, category, address, email } = business;
+
   return (
-    <>
-      <h2 className={styles.title}>Description</h2>
-      <p className={styles.sectionDescription}>{about || 'No information'}</p>
-      <h2 className={styles.title}>Gallery</h2>
-      <div className={styles.gallary}>
-        <ul>
-          {images.map(({ url }) => (
-            <li key={_id}>
-              <img className={styles.businessImg} src={url} alt={`business photo`} />
+    <div className={styles.container}>
+      <div className={styles.infoContainer}>
+        <img className={styles.firstImage} src={imageUrls[0]} alt={`${name} business photo`} />
+        <div className={styles.textInfoContainer}>
+          <div className={styles.category}>{category}</div>
+          <h1 className={styles.title}>{name}</h1>
+          <p className={styles.iconsContainer}>
+            <CiLocationOn className={styles.icon} />
+            {address}
+          </p>
+          <p className={styles.iconsContainer}>
+            <HiOutlineMail className={styles.icon} />
+            {email}
+          </p>
+        </div>
+      </div>
+      <div>
+        <h2 className={styles.title}>Description</h2>
+        <p className={styles.description}>{about || "No information"}</p>
+      </div>
+      <div>
+        <h2 className={styles.title}>Gallery</h2>
+        <ul className={styles.gallery}>
+          {imageUrls.map((image: string) => (
+            <li key={image}>
+              <img className={styles.businessImg} src={image} alt={`${name} business photo`} />
             </li>
           ))}
         </ul>
       </div>
-    </>
+    </div>
   );
 };
 
