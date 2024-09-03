aa7cd4206123046ccc0cb33b5c78c8db5d2d4995
Viktorija.K <viktorija.kotaite@gmail.com>
2024-09-03 10:22:54 +0300

    removed outsideClick function to the hook

--- home-service/src/components/others/UserDropdownMenu/UserDropdownMenu.tsx
+++ home-service/src/components/others/UserDropdownMenu/UserDropdownMenu.tsx
@@ -1,45 +1,24 @@
-import { useEffect, useRef } from "react";
+import { useContext, useRef } from "react";
 import styles from "./userDropdownMenu.module.css";
 import classNames from "classnames/bind";
+import { useNavigate } from "react-router-dom";
+import { UserContext } from "@/context/UserContext";
+import useOutsideClick from "@/hooks/useOutsideClick";
+import { getUserDropdownButtons } from "./utils";
 
 const cx = classNames.bind(styles);
-
-export interface DropdownButton {
-  label: string;
-  onClick: () => void;
-  ariaLabel?: string;
-}
 
 interface UserSettingsDropDownProps {
   onClose: () => void;
   isVisible: boolean;
-  buttons: DropdownButton[];
 }
 
-const UserSettingsDropDown = ({ isVisible, onClose, buttons }: UserSettingsDropDownProps) => {
+const UserSettingsDropDown = ({ isVisible, onClose }: UserSettingsDropDownProps) => {
+  const { logout } = useContext(UserContext);
+  const navigate = useNavigate();
   const dropDownRef = useRef<HTMLUListElement | null>(null);
 
-  useEffect(() => {
-    const handleOutsideClick = (event: MouseEvent) => {
-      const target = event.target as HTMLElement;
-
-      if (dropDownRef.current && dropDownRef.current.contains(target)) {
-        return;
-      }
-
-      if (target.getAttribute("data-button") === "settingDropDown") {
-        return;
-      }
-
-      onClose();
-    };
-
-    document.addEventListener("mousedown", handleOutsideClick);
-
-    return () => {
-      document.removeEventListener("mousedown", handleOutsideClick);
-    };
-  }, [onClose]);
+  useOutsideClick(dropDownRef, onClose, "settingDropDown");
 
   return (
     <ul
@@ -47,20 +26,13 @@ const UserSettingsDropDown = ({ isVisible, onClose, buttons }: UserSettingsDropD
       role="menu"
       aria-label="settings menu"
       tabIndex={0}
+      onClick={onClose}
+      onKeyDown={onClose}
       className={cx({ dropDownCotainer: isVisible }, { hidden: !isVisible })}
     >
-      {buttons.map(({ label, ariaLabel, onClick }) => (
+      {getUserDropdownButtons(navigate, logout).map(({ label, onClick }) => (
         <li key={label}>
-          <button
-            className={styles.button}
-            type="button"
-            aria-label={ariaLabel || label}
-            role="menuitem"
-            onClick={() => {
-              onClick();
-              onClose();
-            }}
-          >
+          <button className={styles.button} type="button" aria-label={label} role="menuitem" onClick={onClick}>
             {label}
           </button>
         </li>
