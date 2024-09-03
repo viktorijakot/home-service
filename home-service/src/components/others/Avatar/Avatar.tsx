5da56d6e4ead7aae77c722958d5a8ff6a50a7ce8
Viktorija.K <viktorija.kotaite@gmail.com>
2024-09-03 10:22:02 +0300

    remove buttons prop and utils moved to userDropdown file

--- home-service/src/components/others/Avatar/Avatar.tsx
+++ home-service/src/components/others/Avatar/Avatar.tsx
@@ -3,33 +3,24 @@ import styles from "./avatar.module.css";
 import { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import UserSettingsDropDown from "../UserDropdownMenu/UserDropdownMenu";
-import { getUserDropdownButtons } from "./utils";
 
 interface AvatarProps {
   children: React.ReactNode;
 }
 
 const Avatar = ({ children }: AvatarProps) => {
-  const navigate = useNavigate();
-  const { logout } = useContext(UserContext);
   const [isUserSettingOpen, setIsUserSettingOpen] = useState(false);
 
   const handlesUserSettingOpen = () => {
     setIsUserSettingOpen(!isUserSettingOpen);
   };
 
-  const buttons = getUserDropdownButtons(navigate, logout);
-
   return (
     <>
       <div className={styles.avatar} onClick={handlesUserSettingOpen} data-button="settingDropDown">
         {children}
       </div>
-      <UserSettingsDropDown
-        onClose={() => setIsUserSettingOpen(false)}
-        isVisible={isUserSettingOpen}
-        buttons={buttons}
-      />
+      <UserSettingsDropDown onClose={() => setIsUserSettingOpen(false)} isVisible={isUserSettingOpen} />
     </>
   );
 };
