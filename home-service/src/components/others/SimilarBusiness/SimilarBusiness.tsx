fbfe67cee0d5e1395a65b453a92125614550b0cc
Viktorija.K <viktorija.kotaite@gmail.com>
2024-09-03 19:30:30 +0300

    finished component

--- home-service/src/components/others/SimilarBusiness/SimilarBusiness.tsx
+++ home-service/src/components/others/SimilarBusiness/SimilarBusiness.tsx
@@ -1,35 +1,42 @@
-import styles from './similarBusiness.module.css';
-import { generatePath, Link } from 'react-router-dom';
-import { useBusinesses } from '@/hooks/useBusinesses';
-import { useBusiness } from '@/hooks/useBusinesses';
-import { ROUTES } from '@/router/Routes';
+import styles from "./similarBusiness.module.css";
+import { generatePath, NavLink, useNavigate } from "react-router-dom";
+import { useBusinesses } from "@/hooks/useBusinesses";
+import { ROUTES } from "@/router/Routes";
+import { Business } from "@/types/businesses";
 
-const SimilarBusiness: React.FC = () => {
-  const { data: businesses } = useBusinesses();
-  const { data: currentBusiness } = useBusiness();
+const SimilarBusiness = ({ currentBusiness }: { currentBusiness: Business }) => {
+  const { data } = useBusinesses();
+  const businesses = data ?? [];
 
-  const similarBusiness = businesses?.filter(
+  const similarBusiness = businesses.filter(
     (business) => business.category === currentBusiness?.category && business._id !== currentBusiness._id,
   );
 
   return (
-    <>
+    <div className={styles.container}>
       <h3 className={styles.title}>Similar Business</h3>
       <div className={styles.similarContainer}>
-        {similarBusiness?.map((simBusiness) => (
-          <div className={styles.similarBusiness} key={simBusiness._id}>
-            <img className={styles.businessImg} src={simBusiness.images[0].url} alt={simBusiness.name} />
-            <div className={styles.detailsContainer}>
-              <Link className={styles.similarName} to={generatePath(ROUTES.BUSINESSES_ID, { id: simBusiness._id })}>
-                {simBusiness.name}
-              </Link>
-              <p className={styles.contactPerson}>{simBusiness.contactPerson}</p>
-              <p>{simBusiness.address}</p>
+        {similarBusiness.length > 0 ? (
+          similarBusiness?.map((simBusiness) => (
+            <div className={styles.similarBusiness} key={simBusiness._id}>
+              <img className={styles.businessImg} src={simBusiness.imageUrls[0]} alt={simBusiness.name} />
+              <div className={styles.detailsContainer}>
+                <NavLink
+                  className={styles.similarName}
+                  to={generatePath(ROUTES.BUSINESSES_ID, { id: simBusiness._id })}
+                >
+                  {simBusiness.name}
+                </NavLink>
+                <p className={styles.contactPerson}>{simBusiness.contactPerson}</p>
+                <p>{simBusiness.address}</p>
+              </div>
             </div>
-          </div>
-        ))}
+          ))
+        ) : (
+          <div>There is no simmilar business</div>
+        )}
       </div>
-    </>
+    </div>
   );
 };
 
